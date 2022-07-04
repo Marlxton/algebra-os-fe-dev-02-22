6cec5c4321a42b86d0adab4b2ad5944252074caa
Marlxton <skendertoni2@gmail.com>
2022-07-04 23:07:27 +0200

    add new things

--- src/pages/ChatRoomPage.jsx
+++ src/pages/ChatRoomPage.jsx
@@ -2,10 +2,17 @@ import Button from "../components/Button";
 import { InputElement } from "../components/InputElement";
 import { User } from "../components/User";
 import { useState } from "react";
+import { SettingsForm } from "../components/SettingsForm";
+import { Message } from "../components/Message";
 
 export function ChatRoomPage(props) {
   const [formState, setFormState] = useState({ message: '' });
   const [messages, setMessages] = useState([]);
+  const [settings, setSettings] = useState({});
+
+  const updateSettings = (settings) => {
+    setSettings({ ...settings });
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -13,12 +20,16 @@ export function ChatRoomPage(props) {
     if (formState.message !== '') {
       setMessages((state) => [
         ...state,
-        { ...formState, author: props.user.username },
+        {
+          ...formState,
+          author: props.user.username,
+          settings: { ...settings },
+        },
       ]);
       setFormState({ message: '' });
     }
   }
-  
+
   const handleChange = (event) => {
     setFormState((state) => ({
           ...state,
@@ -27,16 +38,21 @@ export function ChatRoomPage(props) {
   }
 
   const messageElements = messages.map((item, index) => (
-    <div key={index}>
-      <div>{item.author}</div>
-      <div>{item.message}</div>
-    </div>
+    <Message
+      key={index}
+      settings={item.settings}
+      author={item.author}
+      message={item.message}
+    />
   ));
 
   return (
     <div>
       <Button type="button" onClick={props.onSignOut}>Sign out</Button>
       <User src="/image.jpg" username={props.user.username} />
+
+      <SettingsForm user={props.user} onSubmit={updateSettings} />
+
       <form className="form" onSubmit={handleSubmit}>
         <div className="form-field">
           <InputElement name="message"
@@ -49,8 +65,9 @@ export function ChatRoomPage(props) {
           <Button type="submit">Send</Button>
         </div>
       </form>
-      {messageElements}
+      <div className="message-list">
+        {messageElements}
+      </div>
     </div>
   );
 }
-
